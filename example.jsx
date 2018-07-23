87ea85f2786539888e134aad1a4cde51a3fd84a0
Yarden Hochman <yyyarden@gmail.com>
2018-07-23 14:58:58 +0300

    Update example.jsx

--- example.jsx
+++ example.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import LoadIf from './LoadIf';
 
 export default () => (
-<React.Fragment>
-  <h1>Universal Title</h1>
-  <LoadIf.Desktop><h2>Desktop Title<h2></LoadIf.Desktop>
-  <LoadIf.Portrait><h2>Phone Portrait Title!</h2></LoadIf.Portrait>
- </React.Fragment>
+  <React.Fragment>
+    <h1>Universal Title</h1>
+    <LoadIf.Desktop><h2>Desktop Title<h2></LoadIf.Desktop>
+    <LoadIf.Portrait><h2>Phone Portrait Title!</h2></LoadIf.Portrait>
+  </React.Fragment>
   );
